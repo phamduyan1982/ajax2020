2b815ac7814c529f09e59f6f1f9a6023aafada5c
phamduyan1982 <57425728+phamduyan1982@users.noreply.github.com>
2020-05-04 16:03:50 +0700

    Add project files.

---
+++ AJAXtable/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AJAXtable.Models
+{
+    //[Table("Employees")]
+    //public class Employee
+    //{
+    //    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    //    public int ID { set; get; }
+
+    //    [StringLength(255)]
+    //    [Column(TypeName="nvarchar")]
+    //    [Required]
+    //    public string Name { set; get; }
+
+
+    //    public double Salary { set; get; }
+
+
+    //    public bool Status { set; get; }
+    //}
+}
